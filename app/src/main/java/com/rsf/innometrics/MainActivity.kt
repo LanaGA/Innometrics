af847e95173e3feb08a7b4f4efa6dcbfbb8fe02b
LanaGA <l.galieva@innopolis.ru>
2020-06-17 21:37:17 +0300

    added settings page mockup

--- app/src/main/java/com/rsf/innometrics/MainActivity.kt
+++ app/src/main/java/com/rsf/innometrics/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.rsf.innometrics
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.room.Room
 import com.rsf.innometrics.data.db.AppDb
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_app_usage_statistics.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_usage_statistics)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
         val db: AppDb = Room.databaseBuilder(applicationContext, AppDb::class.java, "stats")
                 .fallbackToDestructiveMigration()
@@ -19,5 +25,33 @@ class MainActivity : AppCompatActivity() {
                     .add(R.id.container, MainFragment.newInstance(db))
                     .commit()
         }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, SettingsFragment())
+                    .addToBackStack(null)
+                    .commit()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    //    override fun onContextItemSelected(item: MenuItem?): Boolean {
+//        supportFragmentManager.beginTransaction()
+//                .add(R.id.container, SettingsFragment.newInstance())
+//                .commit()
+//        return super.onContextItemSelected(item)
+//    }
+
 }
